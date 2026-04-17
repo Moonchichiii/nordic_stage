3f67db0f7e67c87df33b2ee0a5130802e9603c5b
Moonchichiii <matgus1983@gmail.com>
2026-04-17 12:58:30 +0200

    feat(events): add Event admin configuration and tests

--- apps/api/events/admin.py
+++ apps/api/events/admin.py
@@ -1,2 +1,19 @@
+from django.contrib import admin
 
-# Register your models here.
+from events.models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin[Event]):
+    list_display = (
+        "name",
+        "slug",
+        "start_at",
+        "end_at",
+        "is_published",
+        "created_at",
+    )
+    search_fields = ("name", "slug", "description")
+    list_filter = ("is_published", "created_at", "updated_at")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("start_at",)
